8a556205899ccfcdae817bb40bf7a6a3a7a32002
mariance95 <mariano.cerda.cabrera@gmail.com>
2024-06-27 08:11:38 -0300

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/midi-magic",
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
 export default nextConfig;
